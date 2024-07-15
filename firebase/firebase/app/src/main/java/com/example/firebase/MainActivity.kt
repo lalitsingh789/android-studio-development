51721acd8e7a2e155c6ecfe998d073fefa36c251
lalitsingh789 <singhlalit789.w017@gmail.com>
2024-07-15 22:34:08 +0530

    add

---
+++ firebase/firebase/app/src/main/java/com/example/firebase/MainActivity.kt
@@ -0,0 +1,243 @@
+package com.example.firebase
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.Visibility
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.firebase.ui.theme.FirebaseTheme
+import com.google.firebase.auth.FirebaseAuth
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
+        setContent {
+            FirebaseTheme {
+                AppNavigate()
+            }
+        }
+    }
+
+    val auth = FirebaseAuth.getInstance()
+
+    fun signIn(email: String, password: String, context: android.content.Context) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(context, "Sign In Successful", Toast.LENGTH_SHORT).show()
+                val user = auth.currentUser
+                println(user?.uid)
+            } else {
+                Toast.makeText(
+                    context,
+                    "Sign In Failed: ${task.exception?.message}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    fun signUp(email: String, password: String, context: android.content.Context) {
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(context, "Sign Up Successful", Toast.LENGTH_SHORT).show()
+                val user = auth.currentUser
+                println(user?.uid)
+            } else {
+                Toast.makeText(
+                    context,
+                    "Sign Up Failed: ${task.exception?.message}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    @Composable
+    fun SignInScreen(navController: NavController) {
+        var email by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+        var pwVisible by remember { mutableStateOf(false) }
+        val context = LocalContext.current
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(25.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                ElevatedCard(
+                    elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+                    modifier = Modifier.padding(25.dp),
+                    colors = CardDefaults.cardColors(containerColor = Color(0xFFEFF4FA))
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.padding(20.dp)
+                    ) {
+                        OutlinedTextField(
+                            value = email,
+                            onValueChange = { email = it },
+                            modifier = Modifier
+                                .padding(vertical = 10.dp)
+                                .fillMaxWidth(),
+                            label = { Text("Email ID") }
+                        )
+                        OutlinedTextField(
+                            value = password,
+                            onValueChange = { password = it },
+                            modifier = Modifier
+                                .padding(vertical = 10.dp)
+                                .fillMaxWidth(),
+                            label = { Text("Password") },
+                            visualTransformation = if (pwVisible) {
+                                VisualTransformation.None
+                            } else {
+                                PasswordVisualTransformation()
+                            },
+
+                        )
+                        Button(
+                            onClick = { signIn(email, password, context) },
+                            modifier = Modifier
+                                .padding(top = 20.dp)
+                                .fillMaxWidth()
+                        ) {
+                            Text(text = "Login", fontSize = 16.sp)
+                        }
+                    }
+                }
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 10.dp)
+                ) {
+                    Text("Don't have an account?")
+                    TextButton(onClick = { navController.navigate("SignUpScreen") }) {
+                        Text("Sign Up")
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun SignUpScreen(navController: NavController) {
+        var email by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+        var pwVisible by remember { mutableStateOf(false) }
+        val context = LocalContext.current
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(25.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            ElevatedCard(
+                elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+                modifier = Modifier.padding(25.dp),
+                colors = CardDefaults.cardColors(containerColor = Color(0xFFEFF4FA))
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.padding(20.dp)
+                ) {
+                    OutlinedTextField(
+                        value = email,
+                        onValueChange = { email = it },
+                        modifier = Modifier
+                            .padding(vertical = 10.dp)
+                            .fillMaxWidth(),
+                        label = { Text("Email ID") }
+                    )
+                    OutlinedTextField(
+                        value = password,
+                        onValueChange = { password = it },
+                        modifier = Modifier
+                            .padding(vertical = 10.dp)
+                            .fillMaxWidth(),
+                        label = { Text("Create Password") },
+                        visualTransformation = if (pwVisible) {
+                            VisualTransformation.None
+                        } else {
+                            PasswordVisualTransformation()
+                        },
+
+                    )
+                    Button(
+                        onClick = { signUp(email, password, context) },
+                        modifier = Modifier
+                            .padding(top = 20.dp)
+                            .fillMaxWidth()
+                    ) {
+                        Text(text = "Sign Up", fontSize = 16.sp)
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun AppNavigate() {
+        val navController = rememberNavController()
+        NavHost(navController = navController, startDestination = "SignInPage") {
+            composable("SignInPage") { SignInScreen(navController) }
+            composable("SignUpScreen") { SignUpScreen(navController) }
+            composable("HomeScreen") { HomeScreen() }
+        }
+    }
+
+
+    @Composable
+    fun HomeScreen() {
+        // Implementation of HomeScreen goes here
+        Text("Welcome to the Home Screen!")
+    }
+}
